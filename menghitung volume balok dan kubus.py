7152114df6f27d61c64c82fec82dea3e30ee2c25
syaifudinimamanshori <113626164+syaifudinimamanshori@users.noreply.github.com>
2022-09-15 23:38:40 +0700

    Add files via upload

---
+++ menghitung volume balok dan kubus.py
@@ -0,0 +1,28 @@
+# # # #Menghitung volume balok dinamis
+print("menghitung volume balok")
+p =int(input("masukkan panjang = "))
+l =int(input("masukkan lebar ="))
+t =int(input("masukkan tinggi ="))
+volume = p*l*t
+print("hasil = ",volume)
+
+# # #Menghitung volume balok statis
+p =10
+l =5
+t =2
+volume =p*l*t
+print("hasil =",volume)
+
+
+# #Menghitung volume kubus dinamis
+print("Menghitus volume kubus")
+sisi = int(input("nilai sisi ="))
+volume = sisi*sisi*sisi 
+print ("hasil =",volume)
+
+
+#Menghitung volume kubus statis
+print("Menghitung volume kubus")
+s =10
+volume =s*s*s
+print ("hasil = ",volume)
